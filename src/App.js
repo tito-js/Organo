7b2b0a6b469c9b03f2777b280bd6b734ad52785c
tito-mov <titozinnz@gmail.com>
2023-09-18 22:20:54 -0300

    03. Interagindo com o usuário
    -> Controlar inputs utilizando value e onChange;
    
    ->Gerenciar o estado de um componente, utilizando o useState;
    
    ->Trabalhar com props que são funções.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+import { useState } from "react";
 import { Banner } from "./Componentes/Banner/Banner";
 import Formulario from "./Componentes/Formulario/Index";
 
 
 function App() {
+
+  const [colaboradores, setColaboradores] = useState([])
+
+  const aoNovoColaboradorAdicionado = (colaborador) => {
+    console.log(colaborador)
+    setColaboradores([...colaboradores, colaborador])
+  }
+  
+
   return (
     <div className="App">
-      	<Banner />
-		<Formulario />
+        <Banner />
+		    <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
     </div>
   );
 }
--- src/Componentes/ListaSuspensa/Index.js
+++ src/Componentes/ListaSuspensa/Index.js
@@ -1,18 +1,26 @@
 import './ListaSuspensa.css'
+import { useState } from 'react';
 
 const ListaSuspensa = (props) => {
-    console.log(props.itens)
+  const [valorSelecionado, setValorSelecionado] = useState('');
 
-    return (
-        <div className='lista-suspensa'>
-            <label>{props.label}</label>
-            <select required={props.required}>
-                {props.itens.map(item => {
-                    return <option key={item}>{item}</option>
-                })}
-            </select>
-        </div>
-    )
-}
+  const aoAlterarValor = (evento) => {
+    const novoValor = evento.target.value;
+    setValorSelecionado(novoValor);
+    props.aoAlterado(novoValor); 
+  };
 
-export default ListaSuspensa
+  return (
+    <div className='lista-suspensa'>
+      <label>{props.label}</label>
+      <select required={props.required} value={valorSelecionado} onChange={aoAlterarValor}>
+        <option value=""></option>
+        {props.itens.map((item) => {
+          return <option key={item}>{item}</option>;
+        })}
+      </select>
+    </div>
+  );
+};
+
+export default ListaSuspensa;
